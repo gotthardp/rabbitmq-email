4e69c086a76888ffbaae6a366b7afda9de284944
Petr Gotthard <petr.gotthard@centrum.cz>
2015-05-12 09:55:26 +0200

    Enable integration to Jenkins.

--- package.mk
+++ package.mk
@@ -2,4 +2,4 @@ RELEASABLE:=true
 RETAIN_ORIGINAL_VERSION:=true
 DEPS:=rabbitmq-server rabbitmq-erlang-client rabbitmq-gen-smtp
 
-WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_email_tests,[verbose])
+WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_email_tests,[verbose, {report,{eunit_surefire,[{dir,"."}]}}])
