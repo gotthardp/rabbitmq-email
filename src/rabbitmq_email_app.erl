c46ced90734ef0ec8a3e99871df40a7e8f7fc11d
Michael Klishin <michael@clojurewerkz.org>
2017-10-05 11:28:19 +0100

    A typo

--- src/rabbitmq_email_app.erl
+++ src/rabbitmq_email_app.erl
@@ -31,9 +31,6 @@ init([]) ->
         % email to amqp
         [{email_handler, {gen_smtp_server, start_link, [rabbit_email_handler, ServerConfig]},
             permanent, 10000, worker, [rabbit_email_handler]},
-        % amqo to email
+        % amqp to email
         {message_handler_sup, {rabbit_message_handler_sup, start_link, []},
             permanent, 10000, supervisor, [rabbit_message_handler_sup]}]}}.
-
-% end of file
-
