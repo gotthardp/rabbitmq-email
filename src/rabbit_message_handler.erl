3ab71ca07ddd3a64acccf9c962d5d7cc6c599c5a
Luke Bakken <luke@bakken.io>
2021-11-04 12:51:55 -0700

    Make this plugin compatible with RabbitMQ 3.9.x
    Start with the following:
    * Update `erlang.mk` and `rabbitmq-components.mk`
    * Use `gen-smtp/gen_smtp` version `1.1.1`
    * Modify `Makefile` so that it correctly patches
      `deps/gen_smtp/Makefile`
    
    Remove Travis CI configuration, add basic GH workflow
    
    Try curl instead
    
    Remove Windows for now
    
    Required key runs-on
    
    Update based on rabbitmq-server build
    
    Update application to start child specs correctly
    
    Fix test-dist build target
    
    Revert to using application
    
    Finish up test fixes, README updates
    
    Ensure ranch version 2.1.0 is used
    
    Take into account older versions of RabbitMQ prior to this PR being merged - https://github.com/rabbitmq/rabbitmq-server/pull/3685
    
    Update from v3.9.x rabbitmq/rabbitmq-server
    
    One minute of retries
    
    Fixes thanks to @essen

--- src/rabbit_message_handler.erl
+++ src/rabbit_message_handler.erl
@@ -12,40 +12,37 @@
 -include_lib("amqp_client/include/amqp_client.hrl").
 
 -export([start_link/2]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-    terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_continue/2,
+         handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {connection, channel}).
+
+%% This gives RabbitMQ 60 seconds to reach core_started
+-define(DEFAULT_TRIES, 12).
+-define(RETRY_DELAY_MS, 5000).
 
 start_link({VHost, Queue}, Domain) ->
     gen_server:start_link(?MODULE, [{VHost, Queue}, Domain], []).
 
 init([{VHost, Queue}, Domain]) ->
     process_flag(trap_exit, true),
-    {ok, Connection} = amqp_connection:start(#amqp_params_direct{virtual_host=VHost}),
-    try_declaring_queue(Connection, Queue),
-
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-    Subscribe = #'basic.consume'{queue=Queue, consumer_tag=Domain, no_ack=true},
-    #'basic.consume_ok'{} = amqp_channel:call(Channel, Subscribe),
-
-    State = #state{connection=Connection, channel=Channel},
-    {ok, State}.
-
-try_declaring_queue(Connection, Queue) ->
-    {ok, Channel} = amqp_connection:open_channel(Connection),
-    try
-        catch amqp_channel:call(Channel, #'queue.declare'{queue=Queue, durable=true})
-    after
-        catch amqp_channel:close(Channel)
-    end.
-
+    {ok, #state{}, {continue, {try_connect, {VHost, Queue, Domain}}}}.
 
 handle_call(_Msg, _From, State) ->
     {reply, unknown_command, State}.
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
+
+handle_continue({try_connect, Args}, State0) ->
+    {ok, State1} = try_start_connection(Args, ?DEFAULT_TRIES, State0),
+    {noreply, State1}.
+
+handle_info({retry_connect, _Args, 0}, State) ->
+    {stop, connection_failed, State};
+handle_info({retry_connect, Args, RemainingTries}, State0) ->
+    {ok, State1} = try_start_connection(Args, RemainingTries, State0),
+    {noreply, State1};
 
 %% This is the first message received
 handle_info(#'basic.consume_ok'{}, State) ->
@@ -70,6 +67,8 @@ handle_info(Msg, State) ->
     rabbit_log:info("~w", [Msg]),
     {noreply, State}.
 
+terminate(_Reason, #state{connection=undefined, channel=undefined}) ->
+    ok;
 terminate(_Reason, #state{connection=Connection, channel=Channel}) ->
     amqp_channel:close(Channel),
     amqp_connection:close(Connection),
@@ -77,6 +76,38 @@ terminate(_Reason, #state{connection=Connection, channel=Channel}) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+try_start_connection({VHost, Queue, Domain} = Args, RemainingTries, State0) ->
+    rabbit_log:debug("~p starting connection to vhost '~p'", [?MODULE, VHost]),
+    State1 = case amqp_connection:start(#amqp_params_direct{virtual_host=VHost}) of
+        {error, broker_not_found_on_node} ->
+            rabbit_log:warning("~p: broker_not_found_on_node seen, retrying in 1 second", [?MODULE]),
+            retry_start_connection(Args, RemainingTries),
+            State0;
+        {error, What} ->
+            rabbit_log:error("~p: error starting direct connection to vhost '~p', '~p'", [?MODULE, VHost, What]),
+            retry_start_connection(Args, RemainingTries),
+            State0;
+        {ok, Connection} ->
+            rabbit_log:debug("~p successfully started connection to vhost '~p'", [?MODULE, VHost]),
+            try_declaring_queue(Connection, Queue),
+            {ok, Channel} = amqp_connection:open_channel(Connection),
+            Subscribe = #'basic.consume'{queue=Queue, consumer_tag=Domain, no_ack=true},
+            #'basic.consume_ok'{} = amqp_channel:call(Channel, Subscribe),
+            _State1 = State0#state{connection=Connection, channel=Channel}
+    end,
+    {ok, State1}.
+
+try_declaring_queue(Connection, Queue) ->
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+    try
+        catch amqp_channel:call(Channel, #'queue.declare'{queue=Queue, durable=true})
+    after
+        catch amqp_channel:close(Channel)
+    end.
+
+retry_start_connection(Args, RemainingTries) ->
+    _Ref = erlang:send_after(?RETRY_DELAY_MS, self(), {retry_connect, Args, RemainingTries -1}).
 
 get_content_type(undefined) ->
     {<<"text">>, <<"plain">>};
